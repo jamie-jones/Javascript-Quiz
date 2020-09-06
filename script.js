69b85a89819bc8eac403a3b545ee73ae5cfb7d36
Insomnia <jamie.mar.jones14@gmail.com>
2020-09-05 21:11:08 -0400

    HTML and CSS are made and Javascript is beginning psuedo code

---
+++ script.js
@@ -0,0 +1,17 @@
+// create var for button, highscore, timer, intro wordd
+//create arrays for the questions.
+//Final score page
+//Highscore page
+//set time to 75 seconds and make countdown
+var button = document.querySelector("#button");
+var timer = document.querySelector("#introWords");
+
+// with onClick of Start button,
+// start timer
+// clear page
+// first question appears
+
+// with answer selected, "Right" or "Wrong" will show up
+// text will clear
+// new question will appear
+// if wrong, timer will decrease by ten seconds
